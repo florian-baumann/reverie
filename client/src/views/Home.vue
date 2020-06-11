a4eb80b8f83fc362c15fe9d281aa7328c7167a9c
florian.baumann <com.flo>
2020-06-12 00:31:38 +0200

    login basic

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <HelloWorld/>
+    <Ideapreview/>
+    <Ideapreview/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "../components/HelloWorld.vue"
+import Ideapreview from "../components/idea-preview.vue"
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Ideapreview,
   }
 }
 </script>
+
+<style></style>
---
+++ client/src/views/NavBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#45443e"
+      dark
+    >
+      <div class="d-flex align-center">
+        <!--https://www.freepik.com/free-icon/light-bulb_941295.htm#page=1&query=idea&position=12-->
+        <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          contain
+          src="../assets/idea.png" 
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        small
+        outlined
+        color="#F3F348"
+        class="ma-2"
+      >
+        Login
+      </v-btn>
+
+      <v-btn
+        small
+        color="#F3F348"
+      >
+        <div id="signup_btn">Sign Up</div>
+      </v-btn>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+
+
+    <v-content>
+      <router-view></router-view> <!-- Content -->
+    </v-content> 
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+  #signup_btn {
+    color: #45443e
+  }
+</style>

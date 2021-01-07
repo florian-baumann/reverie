6548a985d5a3e791383b3a300253b890ff51e065
florian.baumann <com.flo>
2021-01-07 23:54:04 +0100

    server change to port 4000

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ require("./routes/comment.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+server const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
